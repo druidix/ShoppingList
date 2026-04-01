79c5c1a38fc3fb3a3640531aeb14e30534b6e860
Kaushik Acharya <kaushik@palebluedot.net>
2026-04-01 16:00:05 -0700

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Kaushik Acharya on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

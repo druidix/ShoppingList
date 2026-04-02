36c6c3ef176e536bba24ca3439749b6015066453
Kaushik Acharya <kaushik@palebluedot.net>
2026-04-01 17:00:51 -0700

    Swift basics - Shopping list

--- ShoppingList/ContentView.swift
+++ ShoppingList/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Image(systemName: "cart.fill")
+                .font(.system(size: 50))
+
+            Text("Shopping List")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("Grocery Time!")
+                .font(.subheadline)
+
+            Button("Add Item") {
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }

9f2fb3ff1412fccc96810af92d07a2ffbfab9cd2
Kaushik Acharya <kaushik@palebluedot.net>
2026-04-27 23:54:33 -0700

    Added @State with a few default items.

--- ShoppingList/ContentView.swift
+++ ShoppingList/ContentView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var items: [ShoppingItem] = [
+            ShoppingItem(
+                name: "Whole Milk",
+                quantity: "0.5 gallon",
+                category: "Dairy"
+            ),
+            ShoppingItem(
+                name: "Bananas",
+                quantity: "4",
+                category: "Produce"
+            ),
+            ShoppingItem(
+                name: "Frosted Mini Wheats",
+                quantity: "1 box",
+                category: "Cereal"
+            )
+        ]
+
     var body: some View {
-        VStack(spacing: 20) {
+        NavigationStack {
+            List(items) { item in
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    //The red dot is just a unicode bullet character
+                    //typed in directly.  Nothing fancy :)
+                    Text("\(item.quantity) • \(item.category)")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .navigationTitle("Shopping List")
             Image(systemName: "cart.fill")
                 .font(.system(size: 50))
 
